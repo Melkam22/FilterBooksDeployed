ec40a219fdd2de9ac73b8624c76eb7fe025b5dba
andreas <andreass.ziegler@gmail.com>
2019-12-13 17:48:28 +0100

    Updates

---
+++ precache-manifest.dcfde6db005a3129b00aa3e4fc180c4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd660329cb24a6a590a13aa1fca5776",
+    "url": "./index.html"
+  },
+  {
+    "revision": "937bee36220a65000c30",
+    "url": "./static/css/main.1f10b9ec.chunk.css"
+  },
+  {
+    "revision": "10d45fe64650b3c2ea59",
+    "url": "./static/js/2.274801f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.274801f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "937bee36220a65000c30",
+    "url": "./static/js/main.9244e398.chunk.js"
+  },
+  {
+    "revision": "8462dfa91ded5908428e",
+    "url": "./static/js/runtime-main.af0dc3d6.js"
+  },
+  {
+    "revision": "c726ebc9d6da6a79ef4b4dab40f8c2eb",
+    "url": "./static/media/raindrops.c726ebc9.jpg"
+  }
+]);
